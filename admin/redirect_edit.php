dbc5c65442f152fcf60bd365382e8df024a00f6e
Sergey <sergey@s-panteleev.ru>
2018-02-03 16:27:17 +0300

    Поправил варнинги

--- admin/redirect_edit.php
+++ admin/redirect_edit.php
@@ -1,12 +1,11 @@
-<?
+<?php
 
 use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
 use Prominado\Redirect\RedirectTable;
 use Prominado\Redirect\Constant;
 
-require($_SERVER["DOCUMENT_ROOT"] . '/bitrix/modules/main/include/prolog_admin_before.php');
-require($_SERVER["DOCUMENT_ROOT"] . '/bitrix/modules/main/include/prolog.php');
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_before.php';
 
 Loc::loadMessages(__FILE__);
 Loader::includeSharewareModule('prominado.redirects');
@@ -14,14 +13,14 @@ Loader::includeSharewareModule('prominado.redirects');
 $ID = ($_REQUEST['ID'] > 0) ? $_REQUEST['ID'] : 0;
 
 $APPLICATION->SetTitle(($ID > 0) ? Loc::getMessage('PROMINADO_REDIRECT_REDIRECT_EDIT') : Loc::getMessage('PROMINADO_REDIRECT_REDIRECT_NEW'));
-require($_SERVER["DOCUMENT_ROOT"] . '/bitrix/modules/main/include/prolog_admin_after.php');
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_after.php';
 
 $tabs = [];
 $tabs[] = [
-	'DIV'   => 'edit1',
-	'TAB'   => Loc::getMessage('PROMINADO_REDIRECT_REDIRECT'),
-	'ICON'  => 'main_user_edit',
-	'TITLE' => Loc::getMessage('PROMINADO_REDIRECT_REDIRECT_SETTINGS')
+    'DIV'   => 'edit1',
+    'TAB'   => Loc::getMessage('PROMINADO_REDIRECT_REDIRECT'),
+    'ICON'  => 'main_user_edit',
+    'TITLE' => Loc::getMessage('PROMINADO_REDIRECT_REDIRECT_SETTINGS')
 ];
 
 $tabControl = new \CAdminForm('prominado_redirect_edit', $tabs);
@@ -29,53 +28,53 @@ $tabControl = new \CAdminForm('prominado_redirect_edit', $tabs);
 $errorText = '';
 
 if (
-	($_SERVER['REQUEST_METHOD'] == 'POST') &&
-	($_POST['save'] <> '' || $_POST['apply'] <> '' || $_POST['save_and_add'] <> '') &&
-	check_bitrix_sessid()
+    ($_SERVER['REQUEST_METHOD'] == 'POST') &&
+    ($_POST['save'] <> '' || $_POST['apply'] <> '' || $_POST['save_and_add'] <> '') &&
+    check_bitrix_sessid()
 ) {
-	$fields = [
-		'OLD_URL' => $_POST['OLD_URL'],
-		'NEW_URL' => $_POST['NEW_URL'],
-		'CODE'    => $_POST['CODE']
-	];
+    $fields = [
+        'OLD_URL' => $_POST['OLD_URL'],
+        'NEW_URL' => $_POST['NEW_URL'],
+        'CODE'    => $_POST['CODE']
+    ];
 
-	if ($ID > 0) {
-		$el = RedirectTable::update($ID, $fields);
-	} else {
-		$el = RedirectTable::add($fields);
-	}
+    if ($ID > 0) {
+        $el = RedirectTable::update($ID, $fields);
+    } else {
+        $el = RedirectTable::add($fields);
+    }
 
-	if ($el->isSuccess()) {
-		$redirect_url_list = '/bitrix/admin/prominado_redirect_list.php?lang=' . LANG;
-		$redirect_url_edit = '/bitrix/admin/prominado_redirect_edit.php?lang=' . LANG;
+    if ($el->isSuccess()) {
+        $redirect_url_list = '/bitrix/admin/prominado_redirect_list.php?lang=' . LANG;
+        $redirect_url_edit = '/bitrix/admin/prominado_redirect_edit.php?lang=' . LANG;
 
-		if ($_POST['save'] <> '') {
-			LocalRedirect($redirect_url_list);
-		} elseif ($_POST["apply"] <> '') {
-			LocalRedirect($redirect_url_edit . '&ID=' . $el->getId() . '&' . $tabControl->ActiveTabParam());
-		} elseif ($_POST["save_and_add"] <> '') {
-			LocalRedirect($redirect_url_edit . '&ID=0&' . $tabControl->ActiveTabParam());
-		}
-	} else {
-		$errorText = implode('<br />', $el->getErrorMessages());
-	}
+        if ($_POST['save'] !== '') {
+            LocalRedirect($redirect_url_list);
+        } elseif ($_POST["apply"] !== '') {
+            LocalRedirect($redirect_url_edit . '&ID=' . $el->getId() . '&' . $tabControl->ActiveTabParam());
+        } elseif ($_POST["save_and_add"] !== '') {
+            LocalRedirect($redirect_url_edit . '&ID=0&' . $tabControl->ActiveTabParam());
+        }
+    } else {
+        $errorText = implode('<br />', $el->getErrorMessages());
+    }
 }
 
 $menu = [];
 $menu[] = [
-	'TEXT'  => Loc::getMessage('PROMINADO_REDIRECT_REDIRECT_LIST'),
-	'LINK'  => '/bitrix/admin/prominado_redirect_list.php?lang=' . LANG,
-	'ICON'  => 'btn_list',
-	'TITLE' => Loc::getMessage('PROMINADO_REDIRECT_REDIRECT_LIST'),
+    'TEXT'  => Loc::getMessage('PROMINADO_REDIRECT_REDIRECT_LIST'),
+    'LINK'  => '/bitrix/admin/prominado_redirect_list.php?lang=' . LANG,
+    'ICON'  => 'btn_list',
+    'TITLE' => Loc::getMessage('PROMINADO_REDIRECT_REDIRECT_LIST'),
 ];
 
 $context = new \CAdminContextMenu($menu);
 $context->Show();
 
 if ($errorText) {
-	$e = new CAdminException([['text' => $errorText]]);
-	$message = new CAdminMessage(Loc::getMessage('PROMINADO_REDIRECT_ERROR'), $e);
-	echo $message->Show();
+    $e = new CAdminException([['text' => $errorText]]);
+    $message = new CAdminMessage(Loc::getMessage('PROMINADO_REDIRECT_ERROR'), $e);
+    echo $message->Show();
 }
 
 $tabControl->BeginPrologContent();
@@ -91,26 +90,26 @@ $tabControl->BeginNextFormTab();
 
 $fields = [];
 if ($ID > 0) {
-	$fields = RedirectTable::getById($ID)->fetch();
+    $fields = RedirectTable::getById($ID)->fetch();
 } else {
-	$fields = $_POST;
+    $fields = $_POST;
 }
 
 $tabControl->AddEditField('OLD_URL', Loc::getMessage('PROMINADO_REDIRECT_OLD_URL'), true, ['size' => 30],
-	$fields['OLD_URL']);
+    $fields['OLD_URL']);
 $tabControl->AddEditField('NEW_URL', Loc::getMessage('PROMINADO_REDIRECT_NEW_URL'), true, ['size' => 30],
-	$fields['NEW_URL']);
+    $fields['NEW_URL']);
 $tabControl->AddDropDownField('CODE', Loc::getMessage('PROMINADO_REDIRECT_CODE'), false, Constant::HTTP_CODES,
-	$fields['CODE']);
+    $fields['CODE']);
 
 $tabControl->Buttons([
-	"disabled"      => false,
-	"btnSave"       => true,
-	"btnCancel"     => true,
-	"btnSaveAndAdd" => true,
+    "disabled"      => false,
+    "btnSave"       => true,
+    "btnCancel"     => true,
+    "btnSaveAndAdd" => true,
 ]);
 
 $tabControl->Show();
 $tabControl->ShowWarnings($tabControl->GetName(), $message);
 
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_admin.php");
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/epilog_admin.php';
--- admin/redirect_list.php
+++ admin/redirect_list.php
@@ -1,12 +1,11 @@
-<?
+<?php
 
 use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
 use Prominado\Redirect\RedirectTable;
 use Prominado\Redirect\Constant;
 
-require($_SERVER["DOCUMENT_ROOT"] . '/bitrix/modules/main/include/prolog_admin_before.php');
-require($_SERVER["DOCUMENT_ROOT"] . '/bitrix/modules/main/include/prolog.php');
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_before.php';
 
 Loc::loadMessages(__FILE__);
 Loader::includeSharewareModule('prominado.redirect');
@@ -17,118 +16,117 @@ $sort = new \CAdminSorting($tableId, 'ID', 'DESC');
 $lAdmin = new \CAdminUiList($tableId, $sort);
 
 if ($lAdmin->EditAction()) {
-	foreach ($_POST['FIELDS'] as $id => $fields) {
-		RedirectTable::update($id, $fields);
-	}
+    foreach ($_POST['FIELDS'] as $id => $fields) {
+        RedirectTable::update($id, $fields);
+    }
 }
 
 if ($ids = $lAdmin->GroupAction()) {
-	foreach ($ids as $id) {
-		switch ($_POST['action_button_' . $tableId]) {
-			case 'delete':
-				RedirectTable::delete($id);
-				break;
-		}
-	}
+    foreach ($ids as $id) {
+        switch ($_POST['action_button_' . $tableId]) {
+            case 'delete':
+                RedirectTable::delete($id);
+                break;
+        }
+    }
 }
 
 $headers = [
-	['id' => 'ID', 'content' => 'ID', 'sort' => 'ID', 'default' => true],
-	[
-		'id'      => 'OLD_URL',
-		'content' => Loc::getMessage('PROMINADO_REDIRECT_OLD_URL'),
-		'sort'    => 'OLD_URL',
-		'default' => true
-	],
-	[
-		'id'      => 'NEW_URL',
-		'content' => Loc::getMessage('PROMINADO_REDIRECT_NEW_URL'),
-		'sort'    => 'NEW_URL',
-		'default' => true
-	],
-	['id' => 'CODE', 'content' => Loc::getMessage('PROMINADO_REDIRECT_CODE'), 'sort' => 'CODE', 'default' => true],
+    ['id' => 'ID', 'content' => 'ID', 'sort' => 'ID', 'default' => true],
+    [
+        'id'      => 'OLD_URL',
+        'content' => Loc::getMessage('PROMINADO_REDIRECT_OLD_URL'),
+        'sort'    => 'OLD_URL',
+        'default' => true
+    ],
+    [
+        'id'      => 'NEW_URL',
+        'content' => Loc::getMessage('PROMINADO_REDIRECT_NEW_URL'),
+        'sort'    => 'NEW_URL',
+        'default' => true
+    ],
+    ['id' => 'CODE', 'content' => Loc::getMessage('PROMINADO_REDIRECT_CODE'), 'sort' => 'CODE', 'default' => true],
 ];
 
 $lAdmin->AddHeaders($headers);
 
 $filterFields = [
-	['id' => 'ID', 'name' => 'ID'],
-	['id' => 'OLD_URL', 'name' => Loc::getMessage('PROMINADO_REDIRECT_OLD_URL')],
-	['id' => 'NEW_URL', 'name' => Loc::getMessage('PROMINADO_REDIRECT_NEW_URL')],
-	['id' => 'CODE', 'name' => Loc::getMessage('PROMINADO_REDIRECT_CODE')],
+    ['id' => 'ID', 'name' => 'ID'],
+    ['id' => 'OLD_URL', 'name' => Loc::getMessage('PROMINADO_REDIRECT_OLD_URL')],
+    ['id' => 'NEW_URL', 'name' => Loc::getMessage('PROMINADO_REDIRECT_NEW_URL')],
+    ['id' => 'CODE', 'name' => Loc::getMessage('PROMINADO_REDIRECT_CODE')],
 ];
 
 $arFilter = [];
 $lAdmin->AddFilter($filterFields, $arFilter);
-$lAdmin->InitFilter($filterFields);
 
 $filter = [];
 foreach ($arFilter as $k => $v) {
-	switch ($k) {
-		case 'OLD_URL':
-		case 'NEW_URL':
-			$filter[$k] = '%' . $v . '%';
-			break;
-		default:
-			$filter[$k] = $v;
-			break;
-	}
+    switch ($k) {
+        case 'OLD_URL':
+        case 'NEW_URL':
+            $filter[$k] = '%' . $v . '%';
+            break;
+        default:
+            $filter[$k] = $v;
+            break;
+    }
 }
 
 $res = RedirectTable::getList([
-	'filter' => $filter,
-	'order'  => [$sort->getField() => $sort->getOrder()],
-	'select' => $lAdmin->GetVisibleHeaderColumns()
+    'filter' => $filter,
+    'order'  => [$sort->getField() => $sort->getOrder()],
+    'select' => $lAdmin->GetVisibleHeaderColumns()
 ]);
 $data = new \CAdminUiResult($res, $tableId);
 $data->NavStart();
 $lAdmin->SetNavigationParams($data);
 
 while ($ar = $data->Fetch()) {
-	$row =& $lAdmin->AddRow($ar['ID'], $ar);
+    $row =& $lAdmin->AddRow($ar['ID'], $ar);
 
-	$row->AddInputField('OLD_URL');
-	$row->AddInputField('NEW_URL');
-	$row->AddSelectField('CODE', Constant::HTTP_CODES);
+    $row->AddInputField('OLD_URL');
+    $row->AddInputField('NEW_URL');
+    $row->AddSelectField('CODE', Constant::HTTP_CODES);
 
-	$actions = [];
-	$actions[] = [
-		'ICON'    => 'edit',
-		'TEXT'    => Loc::getMessage('PROMINADO_REDIRECT_EDIT'),
-		'LINK'    => 'prominado_redirect_edit.php?lang=' . LANGUAGE_ID . '&ID=' . $ar['ID'],
-		'DEFAULT' => true
-	];
-	$actions[] = [
-		'ICON'   => 'delete',
-		'TEXT'   => Loc::getMessage('PROMINADO_REDIRECT_DELETE'),
-		'ACTION' => 'if(confirm(\'' . Loc::getMessage('PROMINADO_REDIRECT_ARE_U_SURE') . '\')) ' . $lAdmin->ActionDoGroup($ar['ID'],
-				'delete')
-	];
-	$row->AddActions($actions);
+    $actions = [];
+    $actions[] = [
+        'ICON'    => 'edit',
+        'TEXT'    => Loc::getMessage('PROMINADO_REDIRECT_EDIT'),
+        'LINK'    => 'prominado_redirect_edit.php?lang=' . LANGUAGE_ID . '&ID=' . $ar['ID'],
+        'DEFAULT' => true
+    ];
+    $actions[] = [
+        'ICON'   => 'delete',
+        'TEXT'   => Loc::getMessage('PROMINADO_REDIRECT_DELETE'),
+        'ACTION' => 'if(confirm(\'' . Loc::getMessage('PROMINADO_REDIRECT_ARE_U_SURE') . '\')) ' . $lAdmin->ActionDoGroup($ar['ID'],
+                'delete')
+    ];
+    $row->AddActions($actions);
 }
 
 $context = [];
 $context[] = [
-	'TEXT'  => Loc::getMessage('PROMINADO_REDIRECT_NEW'),
-	'LINK'  => 'prominado_redirect_edit.php?lang=' . LANGUAGE_ID,
-	'TITLE' => Loc::getMessage('PROMINADO_REDIRECT_NEW'),
-	'ICON'  => "btn_new"
+    'TEXT'  => Loc::getMessage('PROMINADO_REDIRECT_NEW'),
+    'LINK'  => 'prominado_redirect_edit.php?lang=' . LANGUAGE_ID,
+    'TITLE' => Loc::getMessage('PROMINADO_REDIRECT_NEW'),
+    'ICON'  => "btn_new"
 ];
 
 $lAdmin->AddAdminContextMenu($context);
 
 $lAdmin->AddGroupActionTable([
-	'edit'   => true,
-	'delete' => true,
+    'edit'   => true,
+    'delete' => true,
 ]);
 
 $lAdmin->CheckListMode();
 
 $APPLICATION->SetTitle(Loc::getMessage('PROMINADO_REDIRECT_LIST'));
 
-require($_SERVER["DOCUMENT_ROOT"] . '/bitrix/modules/main/include/prolog_admin_after.php');
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_after.php';
 
 $lAdmin->DisplayFilter($filterFields);
 $lAdmin->DisplayList();
 
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_admin.php");
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/epilog_admin.php';
